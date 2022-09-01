83e233e670bdca3368207f3e104037347808a9d2
vinnik4m <g.briceno@uniandes.edu.co>
2022-09-01 15:33:08 -0500

    feature/ent_res_pai se generan las entidades Restaurante y pais sin relaciones.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PaisModule } from './pais/pais.module';
+import { RestauranteModule } from './restaurante/restaurante.module';
+import {PaisEntity} from "./pais/pais.entity";
+import {RestauranteEntity} from "./restaurante/restaurante.entity";
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
-  imports: [],
+  imports: [PaisModule, RestauranteModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'museumuser',
+      password: '123456',
+      database: 'museumsdb',
+      entities: [
+        PaisEntity,
+        RestauranteEntity,
+      ],
+      dropSchema: true,
+      synchronize: true,
+      keepConnectionAlive: true,
+    })],
   controllers: [AppController],
   providers: [AppService],
 })
