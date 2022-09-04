f9eaec574f8d5cb3fe624ba4e680155c3b481764
vinnik4m <g.briceno@uniandes.edu.co>
2022-09-03 22:05:12 -0500

    feature/ent_res_pai se generan las entidades Restaurante y pais sin relaciones.

--- src/pais/pais.module.ts
+++ src/pais/pais.module.ts
@@ -2,8 +2,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from "@nestjs/typeorm";
 import {PaisEntity} from "./pais.entity";
+import { PaisService } from './pais.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([PaisEntity])]
+    imports: [TypeOrmModule.forFeature([PaisEntity])],
+    providers: [PaisService]
 })
 export class PaisModule {}
