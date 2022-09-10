3392a81780231195d28f84a68cd71c63725ad4b3
vinnik4m <g.briceno@uniandes.edu.co>
2022-09-10 00:43:32 -0500

    feature/contr_pai_rest Se crea el controlador pais

--- src/pais/pais.dto.ts
+++ src/pais/pais.dto.ts
@@ -1,7 +1,19 @@
+/* eslint-disable prettier/prettier */
+import {IsNotEmpty, IsString, IsUrl} from 'class-validator';
+
 export class PaisDto {
     readonly codigo: number;
+
+    @IsString()
+    @IsNotEmpty()
     readonly nombre: string;
+
+    @IsString()
+    @IsNotEmpty()
     readonly capital: string;
+
+    @IsUrl()
+    @IsNotEmpty()
     readonly bandera: string;
 
 
