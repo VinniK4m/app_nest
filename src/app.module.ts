d08cd040efcf9d419f55ca05f3e1a072c63fc212
Albeiro Cuadrado <a.cuadrado@uniandes.edu.co>
2022-09-18 03:14:00 -0500

    Agregada asociación restaurante-premio

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,6 +16,7 @@ import { RestauranteEntity } from './restaurante/restaurante.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PremioMichelinEntity } from './premio-michelin/premio-michelin.entity';
 import { RecetaEntity } from './receta/receta.entity';
+import { RestaurantePremioModule } from './restaurante-premio/restaurante-premio.module';
 
 @Module({
   imports: [
@@ -36,6 +37,7 @@ import { RecetaEntity } from './receta/receta.entity';
       synchronize: true,
       keepConnectionAlive: true
     }),
+    RestaurantePremioModule,
   ],
   controllers: [AppController],
   providers: [AppService],
