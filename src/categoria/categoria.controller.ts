079072ed8cb78f6cda8951049e10ea4b9d273d4e
vinnik4m <g.briceno@uniandes.edu.co>
2022-10-01 19:46:45 -0500

    Implementación de Seguridad en los controladores.

--- src/categoria/categoria.controller.ts
+++ src/categoria/categoria.controller.ts
@@ -1,26 +1,36 @@
-import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UseInterceptors } from '@nestjs/common';
-import { BusinessErrorsInterceptor } from 'src/interceptors/interceptor';
-import { CategoriaDTO } from './categoria.dto';
+/* eslint-disable prettier/prettier */
+import {Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UseGuards, UseInterceptors} from '@nestjs/common';
+
 import {CategoriaService} from './categoria.service';
+import {JwtAuthGuard} from "../auth/guards/jwt-auth.guard";
+import {RolesGuard} from "../role/roles.guard";
+import {Roles} from "../role/roles.decorator";
+import {RoleType} from "../role/role";
+import {BusinessErrorsInterceptor} from "../shared/interceptors/business-errors.interceptor";
+import {CategoriaDTO} from "./categoria.dto";
 
 
 @Controller('categorias')
 @UseInterceptors(BusinessErrorsInterceptor)
+@UseGuards(JwtAuthGuard, RolesGuard)
 export class CategoriaController {
     constructor(private readonly categoriaService: CategoriaService) {}
 
     @Get()
+    @Roles(RoleType.USERGET, RoleType.ADMIN)
     async findAll() {
         return await this.categoriaService.findAll();
     }
 
     @Get(':categoriaId')
+    @Roles(RoleType.USERGET, RoleType.ADMIN)
     async findOne(@Param('categoriaId') categoriaId: number) {
         return await this.categoriaService.findOne(categoriaId);
     }
 
     @Post()
     @HttpCode(200)
+    @Roles(RoleType.USERPOST, RoleType.ADMIN)
     async create(@Body() categoriaDTO: CategoriaDTO) {
         return await this.categoriaService.create(categoriaDTO);
     }
@@ -28,6 +38,7 @@ export class CategoriaController {
 
 
     @Put(':categoriaId')
+    @Roles(RoleType.USERPOST, RoleType.ADMIN)
     async update(@Param('categoriaId') categoriaId: number, @Body() categoriaDTO: CategoriaDTO) {
         return await this.categoriaService.update(categoriaId, categoriaDTO);
     }
@@ -35,6 +46,7 @@ export class CategoriaController {
 
     @Delete(':categoriaId')
     @HttpCode(204)
+    @Roles(RoleType.USERDEL, RoleType.ADMIN)
     async delete(@Param('categoriaId') categoriaId: number) {
         return await this.categoriaService.delete(categoriaId);
     }
--- src/producto/producto.controller.ts
+++ src/producto/producto.controller.ts
@@ -1,27 +1,38 @@
-import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UseInterceptors } from '@nestjs/common';
-import { BusinessErrorsInterceptor } from 'src/interceptors/interceptor';
-import { ProductoDTO } from './producto.dto';
+/* eslint-disable prettier/prettier */
+import {Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UseGuards, UseInterceptors} from '@nestjs/common';
+
+
 import {ProductoService} from "./producto.service";
-import {ProductoEntity} from "./producto.entity";
+import {JwtAuthGuard} from "../auth/guards/jwt-auth.guard";
+import {RolesGuard} from "../role/roles.guard";
+import {BusinessErrorsInterceptor} from "../shared/interceptors/business-errors.interceptor";
+import {Roles} from "../role/roles.decorator";
+import {RoleType} from "../role/role";
+import {ProductoDTO} from "./producto.dto";
 
 
 @Controller('productos')
+@UseGuards(JwtAuthGuard, RolesGuard)
 @UseInterceptors(BusinessErrorsInterceptor)
+@UseGuards(JwtAuthGuard, RolesGuard)
 export class ProductoController {
     constructor(private readonly productoService: ProductoService) {}
 
     @Get()
+    @Roles(RoleType.USERGET, RoleType.ADMIN)
     async findAll() {
         return await this.productoService.findAll();
     }
 
     @Get(':productoId')
+    @Roles(RoleType.USERGET, RoleType.ADMIN)
     async findOne(@Param('productoId') productoId: number) {
         return await this.productoService.findOne(productoId);
     }
 
 
     @Post()
+    @Roles(RoleType.USERPOST, RoleType.ADMIN)
     @HttpCode(200)
     async create(@Body() productoDTO: ProductoDTO) {
         return await this.productoService.create(productoDTO);
@@ -30,12 +41,14 @@ export class ProductoController {
 
 
     @Put(':productoId')
+    @Roles(RoleType.USERPOST, RoleType.ADMIN)
     async update(@Param('productoId') productoId: number, @Body() productoDTO: ProductoDTO) {
         return await this.productoService.update(productoId, productoDTO);
     }
 
 
     @Delete(':productoId')
+    @Roles(RoleType.USERDEL, RoleType.ADMIN)
     @HttpCode(204)
     async delete(@Param('productoId') productoId: number) {
         return await this.productoService.delete(productoId);
